466f2a160c2a04d8fbcd8d4041bb90aced0f20c7
jaredpek <83465527+jaredpek@users.noreply.github.com>
2024-08-19 06:34:30 +0800

    add fallback page

--- application/src/app/profile/page.js
+++ application/src/app/profile/page.js
@@ -1,13 +1,22 @@
 import { auth } from "@/auth";
 import Layout from "@/components/layout/Layout";
 import ProfileForm from "@/components/profile/ProfileForm";
+import Link from "next/link";
 
 export default async function Page() {
-  const {user} = await auth();
+  const {user} = await auth() || {};
 
-  return !!user && <>
+  return <>
     <Layout title="Profile">
-      <ProfileForm user={user} />
+      {
+        !!user ? 
+        <ProfileForm user={user} /> :
+        <div className="flex gap-1">
+          Please proceed to the 
+          <Link href="/" className="text-blue-400">home page</Link> 
+          to login!
+        </div>
+      }
     </Layout>
-  </> 
+  </>
 }
